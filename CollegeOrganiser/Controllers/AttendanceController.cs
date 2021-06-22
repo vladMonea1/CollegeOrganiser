87463373adefd9e5588e99a88e7b90bae845aee1
Vlad Monea <vm.business.stuff@gmail.com>
2021-06-22 03:21:27 +0300

    3.0.1 Added My Courses for teachers and tweaks for Attendances

--- CollegeOrganiser/Controllers/AttendanceController.cs
+++ CollegeOrganiser/Controllers/AttendanceController.cs
@@ -37,7 +37,63 @@ namespace CollegeOrganiser.Controllers
 
             return View(attendance);
         }
-        
+
+        [Route("Course/ViewCourseFeed/{id}")]
+
+        public async Task<IActionResult> ViewCourseFeed(int id)
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+            ModelFeedCurs model = new ModelFeedCurs();
+
+            model.CursurileCareSeTin = await _context.CoursesHeld.Include(c => c.Course).Where(course => course.Course.Id == id).ToListAsync();
+
+            model.Prezente = await _context.CourseAttendances.Where(c => c.User == user && model.CursurileCareSeTin.Contains(c.CourseAttended)).ToListAsync();
+
+            model.CourseId = id;
+
+            return View(model);
+        }
+
+        [Route("Course/PunePrezenta/{id}")]
+
+        public async Task<IActionResult> PunePrezenta(int id)
+        {
+            var cursTinut = _context.CoursesHeld.Include(c => c.Course).FirstOrDefault(ch => ch.Id == id);
+
+            CourseAttendances prezenta = new CourseAttendances
+            {
+                Attended = true,
+                User = _userManager.GetUserAsync(User).Result,
+                CourseAttended = cursTinut
+
+            };
+
+            _context.CourseAttendances.Add(prezenta);
+            _context.SaveChanges();
+
+            var curs = cursTinut.Course;
+
+            return RedirectToAction("ViewCourseFeed", new { id = curs.Id });
+        }
+
+        public async Task<IActionResult> InitiazaPrezenta(int id)
+        {
+            var cursCurent = _context.Courses.FirstOrDefault(c => c.Id == id);
+
+
+
+            CoursesHeld curS = new CoursesHeld()
+            {
+                DateHeld = DateTime.Now,
+                Course = cursCurent,
+
+            };
+            _context.CoursesHeld.Add(curS);
+            _context.SaveChanges();
+
+            return RedirectToAction("ViewCourseFeed", new { id = cursCurent.Id });
+        }
+
 
     }
 }
